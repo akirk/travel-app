caccd5f981f260fb08c7a42e50382095201fd8e3
Alex Kirk <akirk@users.noreply.github.com>
2026-08-01 09:51:47 +0200

    Move attachment offline marker inward

--- templates/trip.php
+++ templates/trip.php
@@ -642,8 +642,8 @@ if ( count( $route_locations ) >= 2 ) {
         }
         .attachment-offline-indicator {
             position: absolute;
-            right: -0.18em;
-            bottom: -0.18em;
+            right: 0.05em;
+            bottom: 0.05em;
             color: #237a3b;
             font-size: 0.78em;
             font-weight: 900;

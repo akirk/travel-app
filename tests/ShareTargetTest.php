63425172d22883101dcafb026588a6674debfb19
Alex Kirk <akirk@users.noreply.github.com>
2026-09-18 10:07:34 +0200

    Reject malformed shared upload metadata

--- tests/ShareTargetTest.php
+++ tests/ShareTargetTest.php
@@ -55,4 +55,19 @@ final class ShareTargetTest extends TestCase {
         self::assertFalse( ShareTarget::is_text_file( [ 'name' => 'photo.jpg', 'type' => 'image/jpeg' ] ) );
         self::assertFalse( ShareTarget::is_text_file( [] ) );
     }
+
+    public function test_rejects_malformed_upload_metadata_without_warnings(): void {
+        self::assertFalse( ShareTarget::is_text_file( [ 'name' => [ 'trip.ics' ], 'type' => 'text/calendar' ] ) );
+        self::assertFalse( ShareTarget::is_text_file( [ 'name' => 'trip.ics', 'type' => [ 'text/calendar' ] ] ) );
+        self::assertFalse( ShareTarget::is_text_file( [ 'name' => 123, 'type' => 'text/calendar' ] ) );
+
+        $nested = ShareTarget::normalize_files( [
+            'name'     => [ [ 'trip.ics' ] ],
+            'type'     => [ 'text/calendar' ],
+            'tmp_name' => [ '/tmp/trip' ],
+            'error'    => [ 0 ],
+            'size'     => [ 10 ],
+        ] );
+        self::assertFalse( ShareTarget::is_text_file( $nested[0] ) );
+    }
 }
